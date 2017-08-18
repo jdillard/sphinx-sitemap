31e3349061c922f76b17d2bf786b7cff1355c3de
Jared Dillard <jdillard@netgate.com>
2017-08-18 01:35:38 -0500

    remove long description for now

--- setup.py
+++ setup.py
@@ -4,14 +4,9 @@ import os
 exec(compile(
     open('sphinx_sitemap/version.py').read(), 'sphinx_sitemap/version.py', 'exec'))
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
-CHANGELOG = open(os.path.join(here, 'CHANGELOG.md')).read()
-
 setup(
     name='sphinx-sitemap',
     description='Sitemap generator for Sphinx',
-    long_description=README + '\n\n' + CHANGELOG,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Topic :: Documentation',
