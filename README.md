3f91be7bf686d52319e1b8151903c6e62ddddbab
Jared Dillard <jdillard@netgate.com>
2017-08-17 19:35:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# sphinx-sitemap
+
+**sphinx-sitemap** is an extension to the [Sphinx documentation tool](http://sphinx-doc.org/) that builds a sitemap.xml for the HTML version of your documentation.
+
+## Installing
+
+To use it, copy the `sphinx_sitemap` directory into your extensions directory or **sys.path**, then add `sphinx_sitemap` to the `extensions` array in your Sphinx **conf.py**.
+
+**sphinx-sitemap** supports Sphinx 1.2 and later, and Python 2.7, 3.3, and 3.4.
+
+## License
+
+**sphinx-sitemap** is made available under a MIT license; see LICENSE for details.
+
+Originally based on the sitemap generator in the [guzzle_sphinx_theme](https://github.com/guzzle/guzzle_sphinx_theme) project licensed under the MIT license.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+
+exec(compile(
+    open('sphinx_sitemap/version.py').read(), 'sphinx_sitemap/version.py', 'exec'))
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGELOG = open(os.path.join(here, 'CHANGELOG.md')).read()
+
+setup(
+    name='sphinx-sitemap',
+    description='Sitemap generator for Sphinx',
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Documentation',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+      ],
+    version=__version__,
+    author='Jared Dillard',
+    author_email='jared.dillard@gmail.com',
+    install_requires=['six', 'sphinx >= 1.2'],
+    url="https://github.com/jdillard/sphinx-sitemap",
+    packages=['sphinx_sitemap'],
+ )
