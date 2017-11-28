5e60aed624f6fcd045cc936f52e1d3f8523e7e0d
Jared Dillard <jdillard@netgate.com>
2017-11-28 16:59:48 -0600

    add long description to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 import os
 
+long_description = open('README.rst' if os.path.exists('README.rst') else 'README.md').read()
 exec(compile(
     open('sphinx_sitemap/version.py').read(), 'sphinx_sitemap/version.py', 'exec'))
 
 setup(
     name='sphinx-sitemap',
     description='Sitemap generator for Sphinx',
+    long_description=long_description,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Topic :: Documentation',
